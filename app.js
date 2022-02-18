709b6362a92149f24eb19e50563edfbbcd21aeb2
Shea Cronin <shealcronin@gmail.com>
2022-02-18 17:48:17 -0500

    Commit project with ability to sign up and log in

---
+++ app.js
@@ -0,0 +1,102 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const mongoDb = process.env.MONGODB_DEV;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const User = mongoose.model(
+    'User',
+    new Schema({
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+    })
+);
+
+const app = express();
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+            if (err) return done(err);
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' });
+            }
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    // passwords match! user logs in
+                    return done(null, user);
+                } else {
+                    // passwords do not match!
+                    return done(null, false, { message: 'Incorrect password' });
+                }
+            });
+        });
+    })
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+// Gives access to currentUser variable in all views
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.get('/', (req, res) => res.render('index', { user: req.user }));
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+app.post('/sign-up', (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) return next(err);
+
+        const user = new User({
+            username: req.body.username,
+            password: hashedPassword,
+        }).save((err) => {
+            if (err) return next(err);
+            res.redirect('/');
+        });
+    });
+});
+app.post(
+    '/log-in',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/',
+    })
+);
+app.get('/log-out', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
